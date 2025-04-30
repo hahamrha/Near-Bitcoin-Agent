aa879081f503133ce832ff8bfa78517a9958f373
Harsh Tyagi <writetoharshtyagi@gmail.com>
2025-04-30 13:45:44 +0530

    feat: Add localhost:3001 under CORS for local agent testing

--- next.config.ts
+++ next.config.ts
@@ -5,12 +5,19 @@ const nextConfig = {
       {
         source: "/api/:path*",
         headers: [
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "http://localhost:3001",
+          },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,POST,PUT,DELETE,OPTIONS",
           },
-          { key: "Access-Control-Allow-Headers", value: "Content-Type" },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization, mb-metadata",
+          },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
         ],
       },
     ];

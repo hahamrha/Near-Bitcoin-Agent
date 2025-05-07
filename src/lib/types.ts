dca34bb0c9bc5031541313053f8f396229b40835
Harsh Tyagi <writetoharshtyagi@gmail.com>
2025-05-07 19:42:39 +0530

    chore: add transaction types

---
+++ src/lib/types.ts
@@ -0,0 +1,47 @@
+export type Transaction = {
+  txid: string;
+  version: number;
+  locktime: number;
+  vin: Array<{
+    txid: string;
+    vout: number;
+    prevout: {
+      scriptpubkey: string;
+      scriptpubkey_asm: string;
+      scriptpubkey_type: string;
+      scriptpubkey_address: string;
+      value: number;
+    };
+    scriptsig: string;
+    scriptsig_asm: string;
+    witness: string[];
+    is_coinbase: boolean;
+    sequence: number;
+  }>;
+  vout: Array<{
+    scriptpubkey: string;
+    scriptpubkey_asm: string;
+    scriptpubkey_type: string;
+    scriptpubkey_address: string;
+    value: number;
+  }>;
+  size: number;
+  weight: number;
+  fee: number;
+  status: {
+    confirmed: boolean;
+    block_height: number;
+    block_hash: string;
+    block_time: number;
+  };
+};
+
+export type ParsedTransaction = {
+  hash: string;
+  from: string;
+  to: string;
+  amount: number; // in BTC
+  fee: number; // in BTC
+  type: "sent" | "received";
+  timestamp: Date;
+};

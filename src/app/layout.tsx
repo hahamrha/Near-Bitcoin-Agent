29f6bb16a9a530fd6ad4b106770ade41ad8f6331
Harsh Tyagi <writetoharshtyagi@gmail.com>
2025-05-07 19:44:34 +0530

    fix: icon name changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,7 +46,7 @@ export const metadata: Metadata = {
   icons: {
     icon: "/favicon.ico",
     shortcut: "/favicon.ico",
-    apple: "/apple-touch-icon.png", // 180x180
+    apple: "/icon.png", // 180x180
   },
 };
 

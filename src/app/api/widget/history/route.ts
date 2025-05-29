8afa65f6f58a1d2233e3606b1e0e659b9fd438ab
Harsh Tyagi <writetoharshtyagi@gmail.com>
2025-05-29 20:32:09 +0530

    feat: add widget/history route

---
+++ src/app/api/widget/history/route.ts
@@ -0,0 +1,25 @@
+import { type NextRequest, NextResponse } from "next/server";
+
+const {
+  BITTE_API_KEY,
+  BITTE_API_URL = "https://ai-runtime-446257178793.europe-west1.run.app",
+} = process.env;
+
+export const dynamic = "force-dynamic";
+export const maxDuration = 60;
+
+export const GET = async (req: NextRequest): Promise<NextResponse> => {
+  const { searchParams } = new URL(req.url);
+  const id = searchParams.get("id");
+  const url = `${BITTE_API_URL}/history?id=${id}`;
+
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${BITTE_API_KEY}`,
+    },
+  });
+
+  const result = await response.json();
+
+  return NextResponse.json(result);
+};

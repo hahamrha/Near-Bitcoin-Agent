f3dd3bf540543071b15d50810ee18cc7f76a8631
0xyshv <writetoyshv@gmail.com>
2025-09-21 08:17:56 +0530

    chore: update tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,11 +67,12 @@ export default function Home() {
 
         <div className="max-w-3xl mx-auto text-center space-y-8 relative z-10 md:pt-16">
           <div className="inline-block">
-            <a href="https://x.com/btc_near/status/1927340551386394847" target="_blank" rel="noreferrer">
-              <Badge className="bg-emerald-900/50 text-emerald-400 hover:bg-emerald-900/70 px-4 py-1.5 text-sm backdrop-blur-sm">
-                v1.0 Release <ArrowUpRight className="inline h-4 w-4 ml-1" />
-              </Badge>
-            </a>
+
+            <Badge className="bg-emerald-900/50 text-emerald-400 hover:bg-emerald-900/70 px-4 py-1.5 text-sm backdrop-blur-sm">
+              v2.0
+              {/* <ArrowUpRight className="inline h-4 w-4 ml-1" /> */}
+            </Badge>
+
           </div>
 
           <div className="space-y-4">
